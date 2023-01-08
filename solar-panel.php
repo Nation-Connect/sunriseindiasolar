e717303a285d833b0e9efae054a91cb0504f22ce
Dipankar1kumar <dipankar.k.s.v.m@gmail.com>
2023-01-08 15:24:09 +0530

    change in solar rooftop

---
+++ solar-panel.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en" class="">
+<?php
+	include 'head.php';
+	?>
+
+<body>
+<?php
+	include 'header.php';
+	?>
+    <div class="breadcumb">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="breadcrumbs">
+                        <a href="index.php">Home <i class="fa fa-angle-right"></i></a>
+                        <a href="#">Solar Panel</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="about-us-area about-inner product">
+        <div class="container">
+            <div class="row">
+                <div class="about-content">
+
+                    <div class="row">
+                        <div class="col-lg-12 col-md-12 col-12">
+                            <div class="single-about">
+                                <div class="section-title">
+                                    <h1 class="text-center mb-5">Solar Roof Top</h1>
+
+                                </div>
+
+                            </div>
+                        </div>
+                        <div class="col-lg-4">
+                            <img src="img/Sunrise india ongrid solar company.jpg">
+                        </div>
+                        <div class="col-lg-4">
+                            <img src="img/Sunrise india ongrid solar company2.jpg">
+                        </div>
+                        <div class="col-lg-4">
+                            <img src="img/Sunrise india offgrid solar company3.jpg">
+                        </div>
+
+                    </div>
+                </div>
+                <ul>
+                    <h5> Solar Roof Top </h5><Br>
+                    <li>Sunrise India solar rooftop / solar plant solutions are designed to convert solar power into
+                        usable DC electricity that is then converted in to readily usable 3-phase AC power. The systems
+                        can also automatically synchronize with the grid or diesel generator voltage, frequency and
+                        phase. The systems can also tie with the local electricity grids to provide credits for
+                        electricity generation and savings on the electricity bills.
+                        Ideal for homes, offices or manufacturing units with unutilized space on roof or terraces which
+                        can be utilized for setting up the solar plants and provide great long term savings.
+                        The durable nature and efficient design of the system ensure easy installation and low
+                        maintenance even in the most harsh environments.
+                        Our rooftop plants have been installed all over the globe, including many Government and Private
+                        Project installed. This experience makes Sunrise India ideal partner to implement rooftop solar
+                        power solutions.
+                    </li>
+                    <h6> Advantage </h6><Br>
+                    <li>sing solar panels ( Solar Roof Top) is a very effective way to generate electricity for indoor
+                        and outdoor
+                        applications without harming the earth.</li>
+
+                    <ul class="pl-3">
+                        <li>The biggest advantage of solar roof top system is that they are cost friendly and saves your
+                            electricity bill by using the renewable source of energy produced by the sun. </li>
+                        <li>The main advantage of solar roof top is that they are very cost-effective and require almost
+                            maintenance and last up to decades if maintained properly. </li>
+                        <li>It is the most effective, economical and a productive way to use electricity without harming
+                            the earth.</li>
+                    </ul>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+    <?php
+	include 'footer.php';
+	include 'foot.php';
+	?>
+</body>
+</html>
