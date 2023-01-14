6b9f165c9bd3a5b9dbca7f8e2b6b57d88f310b4f
Dipankar1kumar <dipankar.k.s.v.m@gmail.com>
2023-01-14 21:35:39 +0530

    changes in about, gallery and project

--- project.php
+++ project.php
@@ -86,76 +86,14 @@
 
 
 			</div>
-		</div>
-	</div>
-
-	<div class="pre-footer">
-		<div class="container">
-			<div class="row">
-				<div class="col-md-4">
-					<div class="footer-contact">
-						h5><strong>Contact Us</strong></p>
-						<ul>
-							<li><i class="fa fa-envelope"></i> <a
-									href="mailtoinfo%40satwiksungreencorporation.html">info@satwiksungreencorporation.com</a>
-							</li>
-							<li><i class="fa fa-phone"></i> <a href="tel:09057652163">+91 90 57 652163</a></li>
-							<li><i class="fa fa-map-marker"></i> <span>105, Solar Arena, Front OF BHAGWATI KACHORI,
-									BAJRANG NAGAR,KOTA(RAJ.). 324001</span></li>
-						</ul>
-					</div>
-				</div>
-				<div class="col-md-4">
-					<div class="footer-center">
-						<p class="copyright"> Copyright © 2019 All Rights Reserved By <a
-								href="https://www.satwiksungreencorporation.com/">SATWIK SUNGREEN CORPORATION</a></p>
-						<div class="socials clearfix">
-							<div class="icon"><a href="https://www.facebook.com/"><i class='fa fa-facebook'></i></a>
-							</div>
-							<div class="icon"><a href="https://www.instagram.com/"><i class='fa fa-instagram'></i></a>
-							</div>
-							<div class="icon"><a href="https://www.twitter.com/"><i class='fa fa-twitter'></i></a></div>
-							<div class="icon"><a href="https://youtube.com/"><i class='fa fa-youtube'></i></a></div>
-						</div>
-					</div>
-				</div>
-				<div class="col-md-4">
-					<div class="footer-right">
-						<p><strong>Quick Contact</strong></p>
-						<ul class="menu">
-							<li><a href="#">Solar Panel</a></li>
-							<li><a href="#">Solar Inverters</a></li>
-							<li><a href="#">Solar Batteries</a></li>
-							<li><a href="#">Solar Controllers</a></li>
-						</ul>
-					</div>
-				</div>
-
 			</div>
-		</div>
-	</div>
+			</div>
 
-	<div class="footer-bottom">
-		<p>Designed & Developed By <a href="https://www.gdigitalindia.com/">G Digital Media Solutions India Pvt.
-				Ltd.</a></p>
-	</div>
-	<div class="mob-foot-cta">
-		<div class="col-md-6"><a href="tel:09057652163" target="_blank"><i class="fa fa-phone"></i> Call Now</a></div>
-		<div class="col-md-6"><a href="https://api.whatsapp.com/send?phone=919057652163" target="_blank"><i
-					class="fa fa-whatsapp"></i> Whatsapp</a></div>
-	</div>
-
-	<ul class="cta">
-		<li><a href="tel:09057652163"><span class="fa fa-phone"></span>+91 90 57 652163</a></li>
-		<li><a href="https://api.whatsapp.com/send?phone=919057652163"><span class="fa fa-whatsapp"></span>+91 90 57
-				652163</a></li>
-	</ul>
-	<script src="js/jquery-1.12.4.min.js"></script>
-	<script src="js/bootstrap.min.js"></script>
-	<script src="js/owl.carousel.js"></script>
-	<script src="js/jquery-ui.js"></script>
-	<script src="js/fancybox.js"></script>
-	<script src="js/main.js"></script>
+			<?php
+	include 'footer.php';
+	include 'foot.php';
+	?>
 </body>
 
 </html>
+		
