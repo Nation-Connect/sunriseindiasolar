a378e510a499f77028b56c68017fc7bbaa75a72f
sugandhk <sugandhkumar9@gmail.com>
2023-01-16 01:19:26 +0530

    navbar fixed

--- header.php
+++ header.php
@@ -1,46 +1,67 @@
 <div class="topnav">
-        <div class="container">
-            <div class="row">
-                <div class="col-xs-12 col-sm-12 col-md-6 no-padding">
-                    <div class="contacttopleft">
-                        <div class="logo">
-                            <a href="index.php"><img src="img/logo.png" class="img-responsive" title="solar" alt="solar" /></a>
-                            <div class="mobile-menu"></div>
-                        </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-6 no-padding">
+                <div class="contacttopleft">
+                    <div class="logo">
+                        <a href="index.php"><img src="img/logo.png" class="img-responsive" title="solar" alt="solar" /></a>
+                        <div class="mobile-menu"></div>
                     </div>
                 </div>
-                <div class="col-xs-12 col-sm-12 col-md-6 no-padding">
-                    <div class="contacttopright">
-                        <a href="tel:09412787473"><i class='fa fa-phone'></i>+91 9412787473</a>
-                        <a href="mailto:shyam@sunriseindiasolar.com"><i class='fa fa-envelope'></i> shyam@sunriseindiasolar.com</a>
-
-                    </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-6 no-padding">
+                <div class="contacttopright">
+                    <a href="tel:+919412787473"><i class='fa fa-phone'></i>+91 9412787473</a>
+                    <a href="mailto:shyam@sunriseindiasolar.com"><i class='fa fa-envelope'></i> shyam@sunriseindiasolar.com</a>
                 </div>
             </div>
         </div>
     </div>
-    <header class="header header-sticky">
-        <div class="header-bottom menu-center">
-            <div class="container">
-                <div class="row">
+</div>
+<header class="header header-sticky">
+    <div class="header-bottom menu-center">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12 col-sm-12 col-md-12">
+                    <!-- <nav class="main-menu">
+                        <ul>
+                            <li><a href="index.php">Home</a></li>
+                            <li><a href="solar-rooftop.php">Solar&nbsp;Rooftop</a></li>
+                            <li><a href="solar-street-light.php">Solar&nbsp;Street&nbsp;Light</a></li>
+                            <li><a href="solar-water-pump.php">Solar&nbsp;Water&nbsp;Pump</a></li>
+                            <li><a href="solar-water-heater.php">Solar&nbsp;Water&nbsp;Heater</a></li>
+                            <li><a href="gallery.php">Gallery</a></li>
+                            <li><a href="project.php">Site&nbsp;Project</a></li>
+                            <li><a href="about.php">About&nbsp;Us</a></li>
+                            <li><a href="contact.php">Contact&nbsp;Us</a></li>
+                        </ul>
+                    </nav> -->
 
-                    <div class="col-xs-12 col-sm-12 col-md-12">
-                        <nav class="main-menu">
-                            <ul>
-                                <li><a href="index.php">Home</a></li>
-                                <li><a href="solar-rooftop.php">Solar&nbsp;Rooftop</a></li>
-                                <li><a href="solar-street-light.php">Solar&nbsp;Street&nbsp;Light</a></li>
-                                <li><a href="solar-water-pump.php">Solar&nbsp;Water&nbsp;Pump</a></li>
-                                <li><a href="solar-water-heater.php">Solar&nbsp;Water&nbsp;Heater</a></li>
-                                <li><a href="gallery.php">Gallery</a></li>
-                                <li><a href="project.php">Site&nbsp;Project</a></li>
-                                <li><a href="about.php">About&nbsp;Us</a></li>
-                                <li><a href="contact.php">Contact&nbsp;Us</a></li>
-                                
+
+                    <nav class="navbar navbar-expand-lg navbar-light">
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul class="navbar-nav mr-auto">
+                                <li class="nav-item active">
+                                    <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="solar-rooftop.php">Solar&nbsp;Rooftop</a>
+                                </li>
+                                <li class="nav-item"><a class="nav-link" href="solar-street-light.php">Solar&nbsp;Street&nbsp;Light</a></li>
+                                <li class="nav-item"><a class="nav-link" href="solar-water-pump.php">Solar&nbsp;Water&nbsp;Pump</a></li>
+                                <li class="nav-item"><a class="nav-link" href="solar-water-heater.php">Solar&nbsp;Water&nbsp;Heater</a></li>
+                                <li class="nav-item"><a class="nav-link" href="gallery.php">Gallery</a></li>
+                                <li class="nav-item"><a class="nav-link" href="project.php">Site&nbsp;Project</a></li>
+                                <li class="nav-item"><a class="nav-link" href="about.php">About&nbsp;Us</a></li>
+                                <li class="nav-item"><a class="nav-link" href="contact.php">Contact&nbsp;Us</a></li>
                             </ul>
-                        </nav>
-                    </div>
+                        </div>
+                    </nav>
                 </div>
             </div>
         </div>
-    </header>
+    </div>
+</header>
