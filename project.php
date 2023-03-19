22cd0ea8f4dc8ca238845b7eb3965e000dde0677
Dipankar1kumar <dipankar.k.s.v.m@gmail.com>
2023-03-19 13:15:13 +0530

    Change in project page 2

--- project.php
+++ project.php
@@ -34,7 +34,20 @@
 			margin-top: 50px;
 		}
 	</style>
-
+ <style>
+      table,
+      th,
+      td {
+        border: 1px solid black;
+        border-collapse: collapse;
+        text-align: center;
+        padding: 8px;
+      }
+      th {
+        background-color: #4caf50;
+        color: white;
+      }
+    </style>
 
 
 </head>
@@ -124,11 +137,181 @@
 				<td>6</td>
 				<td>300 KW ON GRID</td>
 			</tr>
+
+			<tr>
+          <td>7</td>
+          <td>Central Special Jail, Bhagalpur</td>
+          <td>1</td>
+          <td>277 KW</td>
+        </tr>
+        <tr>
+          <td>8</td>
+          <td>Beur Jail</td>
+          <td>1</td>
+          <td>150 KW</td>
+        </tr>
+        <tr>
+          <td>9</td>
+          <td>IGIMS, Patna</td>
+          <td>1</td>
+          <td>300 KW</td>
+        </tr>
+        <tr>
+          <td>10</td>
+          <td>High Court, Patna</td>
+          <td>1</td>
+          <td>150 KW</td>
+        </tr>
+        <tr>
+          <td>11</td>
+          <td>Aryabhatta University</td>
+          <td>1</td>
+          <td>72 KW</td>
+        </tr>
+        <tr>
+          <td>12</td>
+          <td>Supaul Court</td>
+          <td>1</td>
+          <td>60 KW</td>
+        </tr>
+        <tr>
+          <td>13</td>
+          <td>Purnea Court</td>
+          <td>1</td>
+          <td>40 KW</td>
+        </tr>
+        <tr>
+          <td>14</td>
+          <td>Katihar Court</td>
+          <td>1</td>
+          <td>30 KW</td>
+        </tr>
+        <tr>
+          <td>15</td>
+          <td>Jamui Court</td>
+          <td>1</td>
+          <td>40 KW</td>
+        </tr>
+        <tr>
+          <td>16</td>
+          <td>Munger Court</td>
+          <td>1</td>
+          <td>40 KW</td>
+        </tr>
+        <tr>
+          <td>17</td>
+          <td>Araria Court</td>
+          <td>1</td>
+          <td>40 KW</td>
+        </tr>
+        <tr>
+          <td>18</td>
+          <td>BSPCP Patna</td>
+          <td>1</td>
+          <td>50 KW</td>
+        </tr>
+		<tr>
+                    <td>19</td>
+                    <td>Vidhan Sabha Patna</td>
+                    <td>1</td>
+                    <td>55 KW</td>
+                </tr>
+                <tr>
+                    <td>20</td>
+                    <td>Sachiwalaya Patna</td>
+                    <td>1</td>
+                    <td>40 KW</td>
+                </tr>
+                <tr>
+                    <td>21</td>
+                    <td>Ichai Bhawan Patna</td>
+                    <td>1 </td>
+                    <td>70 KW</td>
+                </tr>
+                <tr>
+                    <td>22</td>
+                    <td>Community Hall, Patna</td>
+                    <td>1</td>
+                    <td>20 KW</td>
+                </tr>
+                <tr>
+                    <td>23</td>
+                    <td>Patna Municipal</td>
+                    <td>1</td>
+                    <td>20 KW</td>
+                </tr>
+                <tr>
+                    <td>24</td>
+                    <td>Son Bhawan Patna</td>
+                    <td>1 </td>
+                    <td>20 KW</td>
+                </tr>
+                <tr>
+                    <td>25</td>
+                    <td>Nagar Nigam Patna</td>
+                    <td>1</td>
+                    <td>45 KW</td>
+                </tr>
+                <tr>
+                    <td>26</td>
+                    <td>A.N. Sinha Institute, Patna</td>
+                    <td>1</td>
+                    <td>55 KW</td>
+                </tr>
+                <tr>
+                    <td>27</td>
+                    <td>Women's ITI College, Patna</td>
+                    <td>1</td>
+                    <td>35 KW</td>
+                </tr>
+                <tr>
+                    <td>28</td>
+                    <td>ITI College, Motihari</td>
+                    <td>1 </td>
+                    <td>30 KW</td>
+                </tr>
+                <tr>
+                    <td>29</td>
+                    <td>ITI College, Betiah</td>
+                    <td>1</td>
+                    <td>38 KW</td>
+                </tr>
+                <tr>
+                    <td>30</td>
+                    <td>ITI College, Muzzafarpur</td>
+                    <td>1</td>
+                    <td>20 KW</td>
+                </tr>
+				<tr>
+            <td>31</td>
+            <td>ITI College, Sitamarhi</td>
+            <td>1</td>
+            <td>20 KW</td>
+          </tr>
+          <tr>
+            <td>32</td>
+            <td>Darbhanga University</td>
+            <td>1</td>
+            <td>150 KW</td>
+          </tr>
+          <tr>
+            <td>33</td>
+            <td>Patna Museum</td>
+            <td>1</td>
+            <td>150 KW</td>
+          </tr>
+          <tr>
+            <td>34</td>
+            <td>Central Jail, Ranchi</td>
+            <td>1 </td>
+            <td>82 KW</td>
+          </tr>
 		</tbody>
-	</table>
+	</table> <br>
+	
 
-
-
+	
+    
 
 
 
