2d67bd4bdee44484639118587a43c777a8fb8b2a
Dipankar1kumar <dipankar.k.s.v.m@gmail.com>
2024-07-06 18:38:25 +0530

    manpower

---
+++ manpowerproject.php
@@ -0,0 +1,197 @@
+<!DOCTYPE html>
+<html lang="en" class="">
+
+<head>
+	<title>SUNRISE INDIA</title>
+	<?php
+	include 'head.php';
+	?>
+
+
+
+<meta charset="UTF-8">
+	<title>Project Work Table</title>
+	<style>
+		table {
+			border-collapse: collapse;
+			width: 100%;
+			max-width: 800px;
+			margin: 0 auto;
+		}
+		th, td {
+			border: 1px solid #ddd;
+			padding: 8px;
+			text-align: center;
+		}
+		th {
+			background-color: #f2f2f2;
+		}
+		tr:nth-child(even) {
+			background-color: #f9f9f9;
+		}
+		h1 {
+			text-align: center;
+			margin-top: 50px;
+		}
+	</style>
+ <style>
+      table,
+      th,
+      td {
+        border: 1px solid black;
+        border-collapse: collapse;
+        text-align: center;
+        padding: 8px;
+      }
+      th {
+        background-color: #4caf50;
+        color: white;
+      }
+    </style>
+
+
+</head>
+
+<body>
+<?php
+	include 'header.php';
+	?>
+	<div class="breadcumb">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+					<div class="breadcrumbs">
+						<a href="index.php">Home <i class="fa fa-angle-right"></i></a>
+						<a href="#">Solar Projects in Bihar</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+
+
+
+
+
+	<h1>List of Deployed Manpower Projects</h1>
+
+<table>
+    <thead>
+        <tr>
+            <th scope="col">Serial Number</th>
+            <th scope="col">Order Number</th>
+            <th scope="col">Total Schools</th>
+            <th scope="col">Project Allocated</th>
+            <th scope="col">Project Location</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>1</td>
+            <td>2682</td>
+            <td>100</td>
+            <td>100 ICT instructors and 100 Night Guards</td>
+            <td>Muzaffarpur</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>2803</td>
+            <td>50</td>
+            <td>50 ICT instructors and 100 ICT instructors & 3000 Computers</td>
+            <td>Muzaffarpur</td>
+        </tr>
+        <tr>
+            <td>3</td>
+            <td>441</td>
+            <td>80</td>
+            <td>80 ICT instructors, 80 Night Guards, and 1600 Computers</td>
+            <td>Samastipur</td>
+        </tr>
+    </tbody>
+</table>
+
+	<div class="about-us-area">
+		<div class="container">
+			<div class="row">
+				<div class="">
+
+					<div class="row">
+						<div class="col-lg-12 col-md-12 col-12">
+							<div class="single-about">
+								<div class="section-title">
+									<h1 class="text-center mb-5"> Images of Our Manpower Deployement</h1>
+
+								</div>
+
+							</div>
+
+						</div>
+						<div class="group" data-lightgallery="group">
+							<div class="row">
+
+								
+								<div class="col-lg-3">
+									<div class="gallery-item"><a data-lightgallery="item" href="img/sunrisemanpower/manpower (1).jpg"><img src="img/sunrisemanpower/manpower (1).jpg"> </a></div>
+								</div>
+								<div class="col-lg-3">
+									<div class="gallery-item"><a data-lightgallery="item" href="img/sunrisemanpower/manpower (2).jpg"><img src="img/sunrisemanpower/manpower (2).jpg"> </a></div>
+								</div>
+                                <div class="col-lg-3">
+									<div class="gallery-item"><a data-lightgallery="item" href="img/sunrisemanpower/manpower (3).jpg"><img src="img/sunrisemanpower/manpower (3).jpg"> </a></div>
+								</div>
+                                <div class="col-lg-3">
+									<div class="gallery-item"><a data-lightgallery="item" href="img/sunrisemanpower/manpower (4).jpg"><img src="img/sunrisemanpower/manpower (4).jpg"> </a></div>
+								</div>
+								
+								
+
+								
+						</div>
+
+
+					</div>
+				</div>
+
+
+			</div>
+		</div>
+	</div>
+	</div>
+
+
+
+
+
+
+
+
+
+
+	
+        
+			
+
+
+
+
+
+
+
+
+
+
+
+
+
+							
+						
+
+			<?php
+	include 'footer.php';
+	include 'foot.php';
+	?>
+</body>
+
+</html>
+		
